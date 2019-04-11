47db12b56347c833f8eb7fce5c715a0ffc4fdaaf
zimingg <zimingg@uoregon.edu>
2019-04-10 20:53:00 -0700

    changed variable name

--- Bandwidth.py
+++ Bandwidth.py
@@ -6,7 +6,7 @@ import sys
 def MeasureBandwidth(url):
     #check if the input string is an url
     try:
-        req = urllib.request.Request(url)
+        the_url = urllib.request.Request(url)
     except:
         print("invalid input!")
         return -1
@@ -16,14 +16,14 @@ def MeasureBandwidth(url):
 
     #open the url
     try:
-        f = urllib.request.urlopen(req)
+        url_file = urllib.request.urlopen(the_url)
     except urllib.error.HTTPError as e:
         print("Invalid url!")
         print("Error Number: {}, Reason: {}".format(e.code, e.reason))
         return -1
 
     #load the file
-    f = f.read()
+    url_file = url_file.read()
 
     end = time.time() #end record time
 
@@ -31,12 +31,12 @@ def MeasureBandwidth(url):
     time_used = end-start
 
     #get the size of the file and convert byte to kb
-    size = sys.getsizeof(f)//1000
+    size = sys.getsizeof(url_file)/1000
 
     #get the speed
     speed = size//time_used
 
-    return speed
+    return int(speed)
 
 
 def main():
@@ -46,7 +46,7 @@ def main():
     else:
         speed = MeasureBandwidth(sys.argv[1])
         if speed!=-1: 
-            print("Bandwidth: {}kb/s ".format(int(speed)))
+            print("Bandwidth: {}kb/s ".format(speed))
         
 
 
